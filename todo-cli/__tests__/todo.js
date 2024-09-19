f4b85368a63a0548b1deb3f5459f263a0c0beb8b
kailas <kailasnath727@gmail.com>
2024-09-19 14:33:05 +0530

    add pre-commit hook to run tests

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,34 +1,29 @@
-const todoList = require('../todo');
+const todoList = require("../todo");
 
-const {all, markAsComplete, add} = todoList();
+const { all, markAsComplete, add } = todoList();
 
 describe("Todo List Test Suite", () => {
-    beforeAll(() => {
-        add(
-            {
-                title: "Test todo",
-                completed: false,
-                dueDate: new Date().toLocaleDateString("en-CA")
-            }
-        );
+  beforeAll(() => {
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
     });
+  });
 
-
-    test("Shouw add new todo", () => {
-        const todoItemCount = all.length;
-        add(
-            {
-                title: "Test todo",
-                completed: false,
-                dueDate: new Date().toLocaleDateString("en-CA")
-            }
-        );
-        expect(all.length).toBe(todoItemCount + 1);
+  test("Shouw add new todo", () => {
+    const todoItemCount = all.length;
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
     });
+    expect(all.length).toBe(todoItemCount + 1);
+  });
 
-    test("Should mark a todo as complete", () => {
-        expect(all[0].completed).toBe(false);
-        markAsComplete(0);
-        expect(all[0].completed).toBe(true);
-    });
-})
+  test("Should mark a todo as complete", () => {
+    expect(all[0].completed).toBe(false);
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
+  });
+});
--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -1,71 +1,69 @@
 const todoList = () => {
-  all = []
+  all = [];
   const add = (todoItem) => {
-    all.push(todoItem)
-  }
+    all.push(todoItem);
+  };
   const markAsComplete = (index) => {
-    all[index].completed = true
-  }
+    all[index].completed = true;
+  };
 
   const overdue = () => {
     // Write the date check condition here and return the array
     // of overdue items accordingly.
-    let res= [];
+    let res = [];
     all.filter((item) => {
       if (item.dueDate < today) {
         res.push(item);
       }
     });
     return res;
-  }
+  };
 
   const dueToday = () => {
     // Write the date check condition here and return the array
     // of todo items that are due today accordingly.
-    let res= [];
+    let res = [];
     all.filter((item) => {
-        if (item.dueDate === today) {
-            res.push(item);
-        }
-        });
+      if (item.dueDate === today) {
+        res.push(item);
+      }
+    });
 
     return res;
-  }
+  };
 
   const dueLater = () => {
     // Write the date check condition here and return the array
     // of todo items that are due later accordingly.
-    let res= [];
+    let res = [];
     all.filter((item) => {
-        if (item.dueDate > today) {
-            res.push(item);
-        }
-        });
+      if (item.dueDate > today) {
+        res.push(item);
+      }
+    });
     return res;
-  }
+  };
 
   const toDisplayableList = (list) => {
     // Format the To-Do list here, and return the output string
     // as per the format given above.
     res = [];
-    list.forEach(element => {
-        if (element.completed && element.dueDate != today) {
-            res.push("[x] " + element.title + " " + element.dueDate);
-        } else if (!element.completed && element.dueDate != today){
-            res.push("[ ] " + element.title + " " + element.dueDate);
-        }
+    list.forEach((element) => {
+      if (element.completed && element.dueDate != today) {
+        res.push("[x] " + element.title + " " + element.dueDate);
+      } else if (!element.completed && element.dueDate != today) {
+        res.push("[ ] " + element.title + " " + element.dueDate);
+      }
 
-        if (element.completed && element.dueDate === today) {
-            res.push("[x] " + element.title);
-        } else if (!element.completed && element.dueDate === today){
-            res.push("[ ] " + element.title);
-        }
-
+      if (element.completed && element.dueDate === today) {
+        res.push("[x] " + element.title);
+      } else if (!element.completed && element.dueDate === today) {
+        res.push("[ ] " + element.title);
+      }
     });
 
     return res.join("\n");
-
-  }
+  };
 
   return {
     all,
@@ -74,7 +72,7 @@ const todoList = () => {
     overdue,
     dueToday,
     dueLater,
-    toDisplayableList
+    toDisplayableList,
   };
 };
 
