e27df833c5a739db66f5f0f735e9d09ad28ea5f2
kailas <kailasnath727@gmail.com>
2024-10-03 21:03:27 +0530

    completed milestone changed todo-cli to todo_cli

---
+++ todo-cli-OG/__tests__/todo.js
@@ -0,0 +1,65 @@
+/* eslint-disable no-undef */
+const todoList = require("../todo");
+
+const { all, markAsComplete, add } = todoList();
+
+describe("Todo List Test Suite", () => {
+  beforeAll(() => {
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+  });
+
+  //test that checks creating a new todo
+  test("Shouw add new todo", () => {
+    const todoItemCount = all.length;
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+    expect(all.length).toBe(todoItemCount + 1);
+  });
+
+  //marking a todo as complete
+  test("Should mark a todo as complete", () => {
+    expect(all[0].completed).toBe(false);
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
+  });
+
+  // //retrieval of overdue items
+  test("Retrieval of overdue items", () => {
+    const todoItemCount = all.length;
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toDateString("en-CA") - 1,
+    });
+    expect(all.length).toBe(todoItemCount + 1);
+  });
+
+  // //retrievval of due today items
+  test("Retrieval of due today items", () => {
+    const todoItemCount = all.length;
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+    expect(all.length).toBe(todoItemCount + 1);
+  });
+
+  // //retrieval of due later items
+  test("Retrieval of due later items", () => {
+    const todoItemCount = all.length;
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toDateString("en-CA") + 1,
+    });
+    expect(all.length).toBe(todoItemCount + 1);
+  });
+});
